a9ca17d7029282681f6d755426878095ff089566
DESKTOP-3JQOKBE\Batch 9 <gracia.masengi@gmail.com>
2024-02-19 14:57:41 +0700

    first commit

---
+++ ProjectA/Program.cs
@@ -0,0 +1,33 @@
+﻿// top level 
+// Console.WriteLine("Hello, World!");
+
+class Program{ //acuan pertama program
+    static void Main()
+    {
+        Person gracia = new Person();
+        gracia.name = "Gracia";
+        gracia.gender = "Female";
+        gracia.age = 22;
+        
+        Console.WriteLine(gracia.name);
+        Console.WriteLine(gracia.gender);
+        Console.WriteLine(gracia.age);
+
+        gracia.SayHi();
+        gracia.Eat();
+    }
+}
+
+class Person{
+    public string name;
+    public string gender;
+    public int age;
+    public void SayHi()
+    {
+        Console.WriteLine("Hi!");
+    }
+    public void Eat()
+    {
+        Console.WriteLine("Eating...");
+    }
+}
---
+++ ProjectB/Program.cs
@@ -0,0 +1,25 @@
+﻿// naming convention: class & method PascalCase, variable camelCase
+
+class Program{
+    static void Main()
+    {
+        Calculator calc = new Calculator();
+        int additionResult = calc.Addition(2,5);
+        int subtractionResult = calc.Subtraction(10,3);
+
+        Console.WriteLine($"2 + 5 = {additionResult}");
+        Console.WriteLine($"10 - 3 = {subtractionResult}");
+    }
+}
+class Calculator{
+    public int Addition(int number1, int number2)
+    {
+        return number1 + number2;
+    }
+
+    public int Subtraction(int number1, int number2)
+    {
+        return number1 - number2;
+    }
+}
+
